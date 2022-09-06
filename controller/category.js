4c958591d087b3063b6f31ebf2b9e98a67c36029
Hui Seong Jeong <gmltjd6801@gmail.com>
2022-09-06 17:20:37 +0900

    카테고리 DB 저장 기능

--- controller/category.js
+++ controller/category.js
@@ -2,19 +2,39 @@ const { Category } = require('../models/Category')
 
 const category = {
     saveCategory: async (req, res) => {
-        const category = new Category(req.body);
-        //MongoDB에서 오는 method, 정보들이 category model에 저장
-        category.save((err) => {
-            //저장할 때 err 발생 시 json으로 반환
-            if (err) return res.json({success: false, err})
-            console.log(("category DB 저장 완료"))
-            return res.json(category)
-                .status(200)
-        })
+        Category.findOne({name: req.params.keyword}, async(err, result) => {
+            let category;
+            if (result === null) {
+                category = new Category(req.body);
+                //MongoDB에서 오는 method, 정보들이 category model에 저장
+                category.save((err) => {
+                    //저장할 때 err 발생 시 json으로 반환
+                    if (err) return res.json({success: false, err})
+                    console.log(("category DB 저장 완료"))
+                    return res.json(category)
+                        .status(200)
+                });
+            } else {
+                console.log(req.body.children)
+                Category.update(
+                    {name: result.name},
+                    {$push: {children: req.body.children}},
+                    (err) => {
+                        if (err) {
+                            window.alert("저장 실패");
+                            console.log(err)
+                        }
+                        console.log(("category DB 중복 저장 완료"))
+                        return res.json(result)
+                            .status(200)
+                    });
+            };
+        });
+
     },
     findParentCategory: async (req, res) => {
-        Category.find({}, async (err, result) => {
-            res.json({categories: result});
+        Category.find({}, async (err, categories) => {
+            res.json({categories});
         })
     },
     findChildCategoryByParent: async (req, res) => {
