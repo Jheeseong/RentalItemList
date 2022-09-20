9930589c41b8856f46f4482aadb559991134792f
강재민 <71342315+kangjaemingit@users.noreply.github.com>
2022-09-20 17:14:30 +0900

    merge into main 'huiseong'

--- Frontend/public/javaScripts/myRental.js
+++ Frontend/public/javaScripts/myRental.js
@@ -1,3 +1,23 @@
+const tabItem = document.querySelectorAll(".tab-container__item");
+const tabContent = document.querySelectorAll(".content-container__content");
+
+tabItem.forEach((item) => {
+    item.addEventListener("click", tabHandler);
+});
+
+function tabHandler(item) {
+    const tabTarget = item.currentTarget;
+    const target = tabTarget.dataset.tab;
+    tabItem.forEach((title) => {
+        title.classList.remove("active");
+    });
+    tabContent.forEach((target) => {
+        target.classList.remove("target");
+    });
+    document.querySelector("#" + target).classList.add("target");
+    tabTarget.classList.add("active");
+}
+
 async function returnItem(rentId, itemId){
     if(window.confirm("물품을 반납하시겠습니까?") === true){
         await fetch('myRental/returnItem/' + rentId + "/" +itemId, {
