c2b4658bd6a983c6fa650d5bd07eb93a293a0bb6
Hui Seong Jeong <gmltjd6801@gmail.com>
2022-09-30 12:03:15 +0900

    aws 이메일 검증 및 기능 구현

--- Backend/config/aws/aws.js
+++ Backend/config/aws/aws.js
@@ -1,48 +1,47 @@
 const AWS = require("aws-sdk");
-AWS.config.loadFromPath('./awsconfig.json');
-
+AWS.config.loadFromPath(__dirname + '/awsconfig.json');
 // Create sendEmail params
-const params = {
-    Destination: { /* required */
-        CcAddresses: [
-            /* more items */
-        ],
-        ToAddresses: [
-            'kjm0551@naver.com',
-            /* more items */
-        ]
-    },
-    Message: { /* required */
-        Body: { /* required */
-            Html: {
-                Charset: "UTF-8",
-                Data: "HTML_FORMAT_BODY"
+module.exports = {
+    awsEMail: (email, randomValue) => {
+        const params = {
+            Destination: { /* required */
+                CcAddresses: [
+                    /* more items */
+                ],
+                ToAddresses: [
+                    email,
+                    /* more items */
+                ]
             },
-            Text: {
-                Charset: "UTF-8",
-                Data: "TEXT_FORMAT_BODY"
-            }
-        },
-        Subject: {
-            Charset: 'UTF-8',
-            Data: 'Test email'
-        }
-    },
-    Source: 'gmltjd6801@naver.com', /* required */
-    ReplyToAddresses: [
-        'gmltjd6801@naver.com',
-        /* more items */
-    ],
-};
+            Message: { /* required */
+                Body: { /* required */
+                    Text: {
+                        Charset: "UTF-8",
+                        Data: "이메일 인증번호는 " + randomValue + " 입니다."
+                    }
+                },
+                Subject: {
+                    Charset: 'UTF-8',
+                    Data: 'Test email'
+                }
+            },
+            Source: 'gmltjd6801@naver.com', /* required */
+            ReplyToAddresses: [
+                'gmltjd6801@naver.com',
+                /* more items */
+            ],
+        };
 
 // Create the promise and SES service object
-const sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+        const sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
 
 // Handle promise's fulfilled/rejected states
-sendPromise.then(
-    function(data) {
-        console.log(data.MessageId);
-    }).catch(
-    function(err) {
-        console.error(err, err.stack);
-    });
+        sendPromise.then(
+            function(data) {
+                console.log(data.MessageId);
+            }).catch(
+            function(err) {
+                console.error(err, err.stack);
+            });
+    }
+}
